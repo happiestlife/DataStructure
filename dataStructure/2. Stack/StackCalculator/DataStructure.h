76620d1376bdce928ea2214fa55e4ef84f8ce2d9
chickenman <programmingStudy9999@gmail.com>
2021-08-19 13:13:04 +0900

    주석처리 완료

--- dataStructure/2. Stack/StackCalculator/DataStructure.h
+++ dataStructure/2. Stack/StackCalculator/DataStructure.h
@@ -3,22 +3,26 @@
 #pragma warning(disable:4996)
 #define SIZE 4
 
-typedef struct StackData {       // 스택안 각 데이터
+// 스택 정보
+typedef struct StackInfo {
+    struct StackData* first;
+    struct StackData* last;
+}StackInfo;
+// 스택안 각 데이터
+typedef struct StackData {       
     char Soperation;
     struct StackData* llink;    // 가장 아랫단을 의미
     struct StackData* rlink;    // 가장 윗단을 의미
 }StackData;
-typedef struct StackInfo {      // 스택 정보
-    struct StackData* first;
-    struct StackData* last;
-}StackInfo;
 
-typedef struct Equation {        // 각 항에 대한 정보
+// 후위 표현으로 변환한 식
+typedef struct {
+    struct Equation* first;     // 후위 표현식 출력을 위한 용도
+    struct Equation* last;      // 데이터 입력/삭제를 위한 용도
+}EquationInfo;
+// 후위 표현식 각 항에 대한 정보
+typedef struct Equation {        
     int operand;
     char operator;
     struct Equation* link;
 }Equation;
-typedef struct {                // 후위 표현으로 변환한 식
-    struct Equation* first;
-    struct Equation* last;
-}EquationInfo;
